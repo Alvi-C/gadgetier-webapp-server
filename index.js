42d0e3142ccf0f77c4f53a9e7a15e964d3e49dbd
Alvi Chowdhury <alvic5050@gmail.com>
2023-11-28 12:14:22 +0600

    all custom middleware added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8000
 const app = express()
 
 
@@ -40,22 +40,90 @@ async function run() {
         /* -----------------all mongoDB connections----------------- */
         const userCollection = database.collection('users');
 
-        /* ----------------------user api---------------------- */
+        /* ----------------------jwt api---------------------- */
 
-        //// user add to database api
-        app.post("/users", async (req, res) => {
+        app.post("/jwt", (req, res) => {
             try {
                 const user = req.body;
+                const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                    expiresIn: "1h",
+                });
+                res.send({ token });
+            } catch (error) {
+                console.error("Error creating JWT:", error);
+                res.status(500).send({ message: "Internal server error" });
+            }
+        });
+
+        // verify token middleware
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verified token: ', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send('unauthorized access');
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        //// verify admin middleware
+        const verifyAdmin = async (req, res, next) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+
+        //// verify moderator middleware
+        const verifyModerator = async (req, res, next) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await userCollection.findOne(query);
+            const isModerator = user?.role === 'moderator';
+            if (!isModerator) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+
+
+
+        /* ----------------------all user related api---------------------- */
+
+        //// add user to database api
+        app.post("/users", async (req, res) => {
+            try {
+                const userData = req.body;
                 // Insert email if the user doesn't exist
-                const query = { email: user.email };
+                const query = { email: userData.email };
                 const existingUser = await userCollection.findOne(query);
                 if (existingUser) {
                     return res.send({ message: "User already exists", insertedId: null });
                 }
-                const result = await userCollection.insertOne(user);
+                const result = await userCollection.insertOne(userData);
                 res.send(result);
             } catch (error) {
                 console.error("Error adding user:", error);
+                res.status(500).send({ message: "Internal server error" });
+            }
+        });
+
+        //// get all user data from database api
+        app.get("/users", async (req, res) => {
+            try {
+                const result = await userCollection.find().toArray();
+                res.send(result);
+            } catch (error) {
+                console.error("Error getting user data:", error);
                 res.status(500).send({ message: "Internal server error" });
             }
         });
